0ad4a80afcc40f404971acadf48f510a8d60becd
wangchen <2213768002@qq.com>
2021-09-05 09:28:11 +0800

    朱树航更改

--- src/main/java/com/bilibili/controller/TestController.java
+++ src/main/java/com/bilibili/controller/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class TestController {
@@ -15,5 +16,11 @@ public class TestController {
     public int sql(){
         Integer integer = jdbcTemplate.queryForObject("select count(*) from book", Integer.class);
         return integer;
+    }
+
+    @ResponseBody
+    @GetMapping("/zsh")
+    public String zhs(){
+        return "zsh";
     }
 }
